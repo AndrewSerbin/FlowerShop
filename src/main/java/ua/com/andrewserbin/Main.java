44c846c00179007bcad747dc6e3ba5e1a0d4787a
Andrew Serbin <andrewserbin01@gmail.com>
2017-07-26 14:30:38 +0300

    Added project.

---
+++ src/main/java/ua/com/andrewserbin/Main.java
@@ -0,0 +1,13 @@
+package ua.com.andrewserbin;
+
+import java.util.ArrayList;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
