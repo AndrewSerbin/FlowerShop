9f9c6d92ca63fe7dc61a9d4e5afafe0acdc66a08
Andrew Serbin <andrewserbin01@gmail.com>
2017-07-26 16:23:13 +0300

    Changed class name.

--- src/main/java/ua/com/andrewserbin/model/entity/boquet/Bouquet.java
+++ src/main/java/ua/com/andrewserbin/model/entity/boquet/Bouquet.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 /**
  * Created by layer on 26.07.2017.
  */
-public class Boquet {
+public class Bouquet {
 
     private Event event;
 
@@ -31,64 +31,67 @@ public class Boquet {
     private Wrapper wrapper;
 
     public static Builder getBuilder() {
-        return new Boquet().new Builder();
+        return new Bouquet().new Builder();
     }
 
     public class Builder {
 
         public Builder setEvent(Event event) {
-            Boquet.this.event = event;
+            Bouquet.this.event = event;
             return this;
         }
 
         public Builder setFreshness(Freshness freshness) {
-            Boquet.this.freshness = freshness;
+            Bouquet.this.freshness = freshness;
             return this;
         }
 
         public Builder setPrice(Price price) {
-            Boquet.this.price = price;
+            Bouquet.this.price = price;
             return this;
         }
 
         public Builder setSize(Size size) {
-            Boquet.this.size = size;
+            Bouquet.this.size = size;
             return this;
         }
 
         public Builder setStemLength(StemLength stemLength) {
-            Boquet.this.stemLength = stemLength;
+            Bouquet.this.stemLength = stemLength;
             return this;
         }
 
         public Builder setFlowers(Flower flower) {
-            Boquet.this.flowers = new ArrayList<>();
-            Boquet.this.flowers.add(flower);
+            Bouquet.this.flowers = new ArrayList<>();
+            Bouquet.this.flowers.add(flower);
             return this;
         }
 
         public Builder setFlowers(ArrayList<Flower> flowers) {
-            Boquet.this.flowers = flowers;
+            Bouquet.this.flowers = flowers;
             return this;
         }
 
         public Builder setBasket(Basket basket) {
-            Boquet.this.basket = basket;
+            Bouquet.this.basket = basket;
             return this;
         }
 
         public Builder setRibbon(Ribbon ribbon) {
-            Boquet.this.ribbon = ribbon;
+            Bouquet.this.ribbon = ribbon;
             return this;
         }
 
         public Builder setWrapper(Wrapper wrapper) {
-            Boquet.this.freshness = freshness;
+            Bouquet.this.freshness = freshness;
             return this;
         }
 
-        public Boquet build() {
-            return Boquet.this;
+        public Bouquet build() {
+
+//            boolean completeBouquetInitialization = event != null
+//            if ()
+            return Bouquet.this;
         }
     }
 
