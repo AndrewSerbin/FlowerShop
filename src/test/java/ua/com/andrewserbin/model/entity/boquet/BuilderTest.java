642987b0834dcf7fb6870367720802352118b7f0
Andrew Serbin <andrewserbin01@gmail.com>
2017-07-26 16:23:43 +0300

    Added builder test.

---
+++ src/test/java/ua/com/andrewserbin/model/entity/boquet/BuilderTest.java
@@ -0,0 +1,34 @@
+package ua.com.andrewserbin.model.entity.boquet;
+
+import org.junit.Test;
+import ua.com.andrewserbin.model.entity.accessory.Basket;
+import ua.com.andrewserbin.model.entity.accessory.Ribbon;
+import ua.com.andrewserbin.model.entity.accessory.Wrapper;
+import ua.com.andrewserbin.model.entity.boquet.util.IncompleteBouquetInitialization;
+import ua.com.andrewserbin.model.entity.plant.Flower;
+import ua.com.andrewserbin.model.entity.plant.FlowerType;
+
+/**
+ * Created by layer on 26.07.2017.
+ */
+public class BuilderTest {
+
+    static String DEFAULT = "Default";
+
+    @Test (expected = IncompleteBouquetInitialization.class)
+    public void build() throws Exception {
+        Bouquet bouquet = Bouquet.getBuilder()
+                .setEvent(Event.ALL)
+                .setFreshness(Freshness.BAD)
+                .setPrice(Price.CHEAP)
+                .setSize(Size.MEDIUM)
+                .setStemLength(StemLength.LONG)
+                .setFlowers(new Flower(DEFAULT, FlowerType.MULTI_FLOWER,
+                        true, true))
+                .setBasket(new Basket(DEFAULT))
+                .setRibbon(new Ribbon(DEFAULT))
+                .setWrapper(new Wrapper(DEFAULT))
+                .build();
+    }
+
+}
