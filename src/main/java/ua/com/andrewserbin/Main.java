74691c3255e642d8aada53614fd3656281eb221a
Andrew Serbin <andrewserbin01@gmail.com>
2017-07-26 16:45:34 +0300

    Cleared main.

--- src/main/java/ua/com/andrewserbin/Main.java
+++ src/main/java/ua/com/andrewserbin/Main.java
@@ -1,17 +1,7 @@
 package ua.com.andrewserbin;
 
-import ua.com.andrewserbin.model.entity.accessory.Basket;
-import ua.com.andrewserbin.model.entity.accessory.Ribbon;
-import ua.com.andrewserbin.model.entity.accessory.Wrapper;
-import ua.com.andrewserbin.model.entity.boquet.*;
-import ua.com.andrewserbin.model.entity.plant.Flower;
-import ua.com.andrewserbin.model.entity.plant.FlowerType;
-
-/**
- * Hello world!
- *
- */
 public class Main {
+
     public static void main( String[] args ) {
 
     }
