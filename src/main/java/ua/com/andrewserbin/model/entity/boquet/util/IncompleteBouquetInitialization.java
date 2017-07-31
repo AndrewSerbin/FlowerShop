41c20c6f2dc1607de7746612482eb0ef84076b41
AndrewSerbin <andrewserbin01@gmail.com>
2017-07-31 19:14:28 +0300

    Added comments.

--- src/main/java/ua/com/andrewserbin/model/entity/boquet/util/IncompleteBouquetInitialization.java
+++ src/main/java/ua/com/andrewserbin/model/entity/boquet/util/IncompleteBouquetInitialization.java
@@ -1,6 +1,7 @@
 package ua.com.andrewserbin.model.entity.boquet.util;
 
 /**
+ * Is created if the initialization was not passed.
  * Created by layer on 26.07.2017.
  */
 public class IncompleteBouquetInitialization extends Exception {
